69837fad59e1e28908701b0dcd46a18ebbce9ecf
hardik-kansal <hkhardik4321@gmail.com>
2026-06-18 16:57:16 +0530

    context-manager got it

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello from project1!")
+# from contextlib import contextmanager
+def contextmanager(func):
+    print("before wrapper")
+
+    class wrapper:
+        def __init__(self, *args):
+            print("init")
+            self.args = args
+            self.g = func(*self.args)
+
+        def __enter__(self):
+            print("enter")
+            return next(self.g)
+
+        def __exit__(self, *args):
+            print("exit")
+            next(self.g)
+            return True
+
+    return wrapper
 
 
-if __name__ == "__main__":
-    main()
+@contextmanager
+def test(name):
+    print("before try")
+    try:
+        yield 1
+        print("after yield")
+    finally:
+        print("bye")
+        yield
+
+
+with test("Sam") as f:
+    print("hi..")
+    print(f)
